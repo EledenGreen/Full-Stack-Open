ce2cf90f79271b68b4eb777baff8e8770c444d8c
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2023-12-31 10:36:08 +0530

    1.4 working

--- Practice/Part-1/Exercises/app_1_1/src/App.jsx
+++ Practice/Part-1/Exercises/app_1_1/src/App.jsx
@@ -16,30 +16,37 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const parts = [
+  {
+    name :'Fundamentals of React',
+    exercises : 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises : 7
+  },
+  {
+    name: 'State of a component',
+    exercises : 14
+  }
+]
   return (
     <>
       <Header course = {course}/>
-      {/* Here, we have to pass the const variable so that 'Const' component can access it  */}
-      <Content part1 = {part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total value = {exercises1 + exercises2 + exercises3} />
+      <Content parts = {parts} />
+
     </>
   )
 }
 
 //const variables are passed as props by the <Content .... />
-const Content = ({part1,part2,part3,exercises1,exercises2,exercises3}) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part no='1' topic={part1} exercise={exercises1} />
-      <Part no='2' topic={part2} exercise={exercises2} />
-      <Part no='3' topic={part3} exercise={exercises3} />
+      <Part no='1' topic={parts[0].name} exercise={parts[0].exercises} />
+      <Part no='2' topic={parts[1].name} exercise={parts[1].exercises} />
+      <Part no='3' topic={parts[2].name} exercise={parts[2].exercises} />
+      <Total value = {parts[0].exercises + parts[1].exercises + parts[2].exercises} /> 
     </div>
   )
 }
