e8aadad5b05e3fe73807582b299c59b3d4c66214
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-01-02 18:26:49 +0530

    1_c complete
    1_c example completed and working

--- Practice/Part-1/part1c/src/App.jsx
+++ Practice/Part-1/part1c/src/App.jsx
@@ -2,17 +2,36 @@ import { useState } from "react"
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+  
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
 
-  console.log('rendering...', counter)
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={decreaseByOne} text='minus' />
+      <Button onClick={setToZero} text='zero' />
+    </div>
+    
   )
 }
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
 
 export default App
