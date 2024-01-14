38d1943e1cd74d21f2bf11db2c6b99152ecbf7db
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-01-14 12:17:11 +0530

    2-d tutorial complete
    starting exercises

--- Practice/Part-2/part_d/part2-notes-frontend/src/App.jsx
+++ Practice/Part-2/part_d/part2-notes-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -8,12 +9,10 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
 
@@ -24,10 +23,16 @@ const App = (props) => {
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important }
 
-    axios
-      .put(url,changedNote)
-      .then(response => {
-        setNotes(notes.map(n => n.id !== id ? n : response.data))
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -38,10 +43,10 @@ const App = (props) => {
       important: Math.random() < 0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
