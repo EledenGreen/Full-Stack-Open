86464e3a04558b551f820696468f88d13d47aa98
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-01-24 14:16:32 +0530

    3-c tutorial < ex 3.15

--- Practice/Part-3/partC/Backend/index.js
+++ Practice/Part-3/partC/Backend/index.js
@@ -12,10 +12,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
@@ -46,12 +42,12 @@ app.get('/api/notes', (req, response) => {
   })
 })
 
-const generateId = () => {
+/*const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
   return maxId + 1
-}
+}*/
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -72,16 +68,46 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if(note){
+        response.json(note)
+      }
+      else
+      {
+        response.status(404).end()
+      }
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndDelete(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findByIdAndDelete(request.params.id)
+    .then(note => {
+      response.json(note)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
@@ -89,4 +115,13 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError')
+  {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(errorHandler)
