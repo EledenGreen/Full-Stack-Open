24273a1a6eb9cfc089f8c57c25dcda07869f8243
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-16 20:36:38 +0530

    tutorial done < ex 4.8

--- Practice/Part-4/Backend/tests/note_api.test.js
+++ Practice/Part-4/Backend/tests/note_api.test.js
@@ -10,13 +10,18 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  console.log('cleared')
+
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note)
+    await noteObject.save()
+    console.log('saved')
+  }
+  console.log('done')
 })
 
 test('notes are returned as json', async () => {
+  console.log('entered test')
   await api
     .get('/api/notes')
     .expect(200)
@@ -88,6 +93,35 @@ test('note without content is not added', async () => {
   const notesAtEnd = await helper.notesInDb()
 
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const notesToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${notesToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  const contents = notesAtEnd.map(r => r.content)
+  assert(!contents.includes(notesToDelete.content))
+
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
 })
 
 after(async () => {
