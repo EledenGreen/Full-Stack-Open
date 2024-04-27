c86d8d38a3f130c491daae60d6283d15e7609194
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-27 09:35:36 +0530

    refactored
    tokenExtractor refactored into middleware.js

--- Practice/Part-4/Backend/controllers/notes.js
+++ Practice/Part-4/Backend/controllers/notes.js
@@ -3,13 +3,7 @@ const Note = require('../models/note')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -29,7 +23,8 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log(request)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'tokken invalid' })
   }
