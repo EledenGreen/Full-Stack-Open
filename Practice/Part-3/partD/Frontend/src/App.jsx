46fd2063be5dab295d4fa77a94e480e699f5e0e1
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-29 17:57:26 +0530

    Revert "tutorial <5.1 implemented"
    This reverts commit 7337b8fbd6697f57be099f9ff1b3b67ed211f44a.

--- Practice/Part-3/partD/Frontend/src/App.jsx
+++ Practice/Part-3/partD/Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 import Notification from './components/Notificatio'
-import loginService from './services/login'
 
 const Footer = () => {
   const footerStyle = {
@@ -25,69 +24,6 @@ const App = (props) => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-  
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      )
-  
-      noteService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
-
-const loginForm = () => (
-  <form onSubmit={handleLogin}>
-    <div>
-      username
-        <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-        />
-    </div>
-    <div>
-      password
-        <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-        />
-    </div>
-    <button type="submit">login</button>
-  </form>
-)
-
-const noteForm = () => (
-  <form onSubmit={addNote}>
-    <input
-      value={newNote}
-      onChange={handleNoteChange}
-    />
-    <button type="submit">save</button>
-  </form>
-)
-
-
 
   useEffect(() => {
     noteService
@@ -95,15 +31,6 @@ const noteForm = () => (
       .then(initialNotes => {
         setNotes(initialNotes)
       })
-  }, [])
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      noteService.setToken(user.token)
-    }
   }, [])
 
   console.log('render', notes.length, 'notes')
@@ -154,15 +81,6 @@ const noteForm = () => (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
-
-      {user === null ?
-        loginForm() :
-        <div>
-          <p>{user.name} logged-in</p>
-          {noteForm()}
-        </div>
-      }
-
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
