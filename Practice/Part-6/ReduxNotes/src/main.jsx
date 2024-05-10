7e9411610e3aa62b8b53900bb9204d3d38a0e9a7
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-10 13:20:46 +0530

    6-a < Vitest installation
    Jest testing will b updated soon.
    so not going to use Jest

---
+++ Practice/Part-6/ReduxNotes/src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.concat(action.payload)
+    return state
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
