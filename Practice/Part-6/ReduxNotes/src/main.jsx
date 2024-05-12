6a2ce830ce20063e0bbfbec731cd6c715ac9fb02
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-12 05:42:51 +0530

    tutorial < 6.3
    App is working

--- Practice/Part-6/ReduxNotes/src/main.jsx
+++ Practice/Part-6/ReduxNotes/src/main.jsx
@@ -1,47 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-
+import App from './App'
+import { noteReducer } from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map(note => 
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
-    </div>
-  )
-}
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
--- Practice/Part-6/ReduxNotes/src/reducers/noteReducer.js
+++ Practice/Part-6/ReduxNotes/src/reducers/noteReducer.js
@@ -21,4 +21,26 @@ const noteReducer = (state = [], action) => {
 
 }
 
-export { noteReducer }
+const generateId = () => 
+  Number((Math.random() * 1000000).toFixed(0))
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+
+export { noteReducer, createNote, toggleImportanceOf }
