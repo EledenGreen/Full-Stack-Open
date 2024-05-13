13e1b4756aaeb8cd71020846229041913e4844ce
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-13 18:40:42 +0530

    tutorial < 6.10
    all test passed

--- Practice/Part-6/ReduxNotes/src/main.jsx
+++ Practice/Part-6/ReduxNotes/src/main.jsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
-import { noteReducer, createNote } from './reducers/noteReducer'
+import noteReducer, { createNote } from './reducers/noteReducer'
 import {filterReducer, filterChange} from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
 
-const store = createStore(reducer)
 
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(filterChange('IMPORTANT'))
