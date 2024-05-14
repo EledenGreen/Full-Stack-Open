a7b7f0c07c4ca2a449f81b6b0f93562de0ba273f
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-14 09:01:15 +0530

    6-c Getting data from backend

---
+++ Practice/Part-6/PartC/src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import App from './App'
+import noteReducer, { createNote, } from './reducers/noteReducer'
+import {filterReducer, filterChange} from './reducers/filterReducer'
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+})
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers from one reduceer from many simple reducers'))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
---
+++ Practice/Part-6/PartC/src/reducers/noteReducer.js
@@ -0,0 +1,42 @@
+import { createSlice, current } from "@reduxjs/toolkit"
+
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState: [],
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      })
+    },
+    toggleImportanceOf(state, action) {
+      const id = action.payload
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+
+      console.log(current(state))
+
+      return state.map(note => 
+        note.id !== id ? note : changedNote
+      )
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return action.payload
+    }
+  },
+})
+
+const generateId = () => 
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export default noteSlice.reducer
