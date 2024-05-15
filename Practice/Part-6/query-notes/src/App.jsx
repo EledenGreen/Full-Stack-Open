9f9522e073d769645091ec165457229455d1542a
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-15 09:39:51 +0530

    toggle importance using React Query implemented

--- Practice/Part-6/query-notes/src/App.jsx
+++ Practice/Part-6/query-notes/src/App.jsx
@@ -1,15 +1,39 @@
-import { useQuery } from "@tanstack/react-query"
-import { getNotes } from "./requests"
+import { useMutation, useQuery, useQueryClient, } from "@tanstack/react-query"
+import { createNote, getNotes, updateNote } from "./requests"
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({ 
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    }
+  })
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updatedNote) => {
+      queryClient.setQueryData(['notes'], (oldData) => {
+        if(!oldData) return []
+        return oldData.map(note => 
+          note.id === updatedNote.id ? updatedNote : note
+        )
+      })
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true })
     console.log(content)
   }
 
   const toggleImportance = (note) => {
+    updateNoteMutation.mutate({...note, important: !note.important })
     console.log('toggle importance of', note.id)
   }
 
