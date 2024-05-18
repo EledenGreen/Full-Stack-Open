2f9c5a18887453d764675bfd56626a59d27c489c
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-18 10:46:43 +0530

    prettier congigured

--- Practice/Part-7/7-d/webpack.config.js
+++ Practice/Part-7/7-d/webpack.config.js
@@ -1,45 +1,46 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
 const config = (env, argv) => {
-    console.log('argv', argv.mode)
+  console.log("argv", argv.mode);
 
-    const backend_url = argv.mode === 'production'
-        ? 'https://notes2023.fly.dev/api/notes'
-        : 'http://localhost:3001/notes'
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: 'main.js'
+  const backend_url =
+    argv.mode === "production"
+      ? "https://notes2023.fly.dev/api/notes"
+      : "http://localhost:3001/notes";
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
+    },
+    devServer: {
+      static: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 3000,
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
         },
-        devServer: {
-            static: path.resolve(__dirname, 'build'),
-            compress: true,
-            port: 3000,
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
-        devtool: 'source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                }
-            ],
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                BACKEND_URL: JSON.stringify(backend_url)
-            })
-        ]
-    }
-}
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url),
+      }),
+    ],
+  };
+};
 
-module.exports = config
+module.exports = config;
