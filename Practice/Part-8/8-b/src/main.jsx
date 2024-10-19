8449cdb438d46efd3c5d8e4f46a861c1473fbbc8
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-19 11:09:34 +0530

    client query impleme < cache implemented

--- Practice/Part-8/8-b/src/main.jsx
+++ Practice/Part-8/8-b/src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 })
 
