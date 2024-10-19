23fb6eb8fd4633c3370c96a69503b0d2215c5c3a
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-19 13:17:49 +0530

    Error handler added

--- Practice/Part-8/8-b/src/App.jsx
+++ Practice/Part-8/8-b/src/App.jsx
@@ -2,18 +2,31 @@ import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
+import Notify from './components/Notify'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  //error handler
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   )
 }
