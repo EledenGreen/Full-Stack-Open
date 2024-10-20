0d6eeacf1314744ae5c5d678d3ba2f716e6c7daf
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-20 16:36:22 +0530

    uri updated

--- Practice/Part-8/8-b/src/main.jsx
+++ Practice/Part-8/8-b/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 })
 
