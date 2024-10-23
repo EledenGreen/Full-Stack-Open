66357950819e7c2a8d483de27eaff4ab74511e3b
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-23 15:55:33 +0530

    8-e repo initialized

---
+++ Practice/Part-8/8-e_frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { useQuery, useApolloClient } from '@apollo/client'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
+import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+  const [token, setToken] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  //error handler
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm />
+    </div>
+  )
+}
+
+export default App
