a22a1979bb00fb9d340ff204e6be748b9472d623
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-30 17:36:33 +0530

    req validation with type guards

--- Practice/Part-9/9-c/src/routes/diaries.ts
+++ Practice/Part-9/9-c/src/routes/diaries.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import express, { Response } from "express";
 import diaryService from "../services/diaryService";
 import { NonSensitiveDiaryEntry } from "../types";
+import toNewDiaryEntry from "../utils";
 
 const router = express.Router();
 
@@ -21,14 +20,18 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-  const addedEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment,
-  });
-  res.json(addedEntry);
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += "Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
