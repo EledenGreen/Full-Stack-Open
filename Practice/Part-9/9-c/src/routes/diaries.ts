57fe0329ddf2661e66ea93cc8d245189ea732188
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-30 20:42:06 +0530

    zod implemented

--- Practice/Part-9/9-c/src/routes/diaries.ts
+++ Practice/Part-9/9-c/src/routes/diaries.ts
@@ -1,7 +1,8 @@
-import express, { Response } from "express";
+import express, { Request, NextFunction, Response } from "express";
 import diaryService from "../services/diaryService";
-import { NonSensitiveDiaryEntry } from "../types";
-import toNewDiaryEntry from "../utils";
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from "../types";
+import { NewEntrySchema } from "../utils";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -19,19 +20,40 @@ router.get("/:id", (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
   try {
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
-    res.json(addedEntry);
+    NewEntrySchema.parse(req.body);
+    next();
   } catch (error: unknown) {
-    let errorMessage = "Something went wrong.";
-    if (error instanceof Error) {
-      errorMessage += "Error: " + error.message;
-    }
-    res.status(400).send(errorMessage);
+    next(error);
   }
-});
+};
+
+const errorMiddleware = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post(
+  "/",
+  newDiaryParser,
+  (
+    req: Request<unknown, unknown, NewDiaryEntry>,
+    res: Response<DiaryEntry>
+  ) => {
+    const addedEntry = diaryService.addDiary(req.body);
+    res.json(addedEntry);
+  }
+);
+
+router.use(errorMiddleware);
 
 export default router;
